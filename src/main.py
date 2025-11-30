85af125da9199c2bb1f36c3a2255ebaf0992b8e0
BlueSkyAndSomeCurses <65008462+BlueSkyAndSomeCurses@users.noreply.github.com>
2025-11-30 18:09:30 +0200

    main notebook + main tex file presentation

---
+++ src/main.py
@@ -0,0 +1,27 @@
+import marimo
+
+__generated_with = "0.18.1"
+app = marimo.App(width="medium")
+
+
+@app.cell(hide_code=True)
+def _(mo):
+    mo.md(r"""
+    # Logistic maps and chaos
+    """)
+    return
+
+
+@app.cell
+def _():
+    return
+
+
+@app.cell
+def _():
+    import marimo as mo
+    return (mo,)
+
+
+if __name__ == "__main__":
+    app.run()
